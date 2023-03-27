d122160901c7a42c2e3354f5091d48ccb90a0c56
Andrea-Baggio <andreabaggio800@gmail.com>
2023-03-27 10:35:12 +0200

    controlli getter e setter

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿public class Evento
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+public class Evento
 {
     public Evento(string titolo, DateTime data, int capienzaMassima, int postiPrenotati)
     {
@@ -8,8 +10,41 @@
         PostiPrenotati = postiPrenotati;
     }
 
-    string Titolo { get; set; }
-    DateTime Data { get; set; }
-    int CapienzaMassima { get;}
-    int PostiPrenotati { get;}
+    public string Titolo
+    {
+        get { return Titolo; }
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                Console.WriteLine("Il titolo non può essere vuoto.");
+            }
+            Titolo = value;
+        }
+    }
+    public DateTime Data
+    {
+        get { return Data; }
+        set
+        {
+            if (value < DateTime.Now)
+            {
+                Console.WriteLine("La data deve essere futura.");
+            }
+            Data = value;
+        }
+    }
+    public int CapienzaMassima
+    {
+        get { return CapienzaMassima; }
+        set
+        {
+            if (value <= 0)
+            {
+                Console.WriteLine("La capienza massima deve essere un numero positivo.");
+            }
+            CapienzaMassima = value;
+        }
+    }
+    public int PostiPrenotati { get;}
 }
