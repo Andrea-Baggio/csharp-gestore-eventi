974c779e1d918814d83293d84836c357c15f846f
Andrea-Baggio <andreabaggio800@gmail.com>
2023-03-27 15:12:36 +0200

    inizio milestone 2

---
+++ Evento.cs
@@ -0,0 +1,77 @@
+﻿using System.Globalization;
+
+public class Evento
+{
+    public string _titolo;
+    public DateTime _data;
+    public int _capienzaMassima;
+    public int PostiPrenotati { get; private set; }
+
+    public Evento(string titolo, DateTime data, int capienzaMassima)
+    {
+        Titolo = titolo;
+        Data = data;
+        CapienzaMassima = capienzaMassima;
+        PostiPrenotati = 0;
+    }
+
+    public string Titolo
+    {
+        get { return _titolo; }
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                Console.WriteLine("Il titolo non può essere vuoto.");
+            }
+            _titolo = value;
+        }
+    }
+    public DateTime Data
+    {
+        get { return _data; }
+        set
+        {
+            if (value < DateTime.Now)
+            {
+                Console.WriteLine("La data inserita non è valida.");
+            }
+            _data = value;
+        }
+    }
+    public int CapienzaMassima
+    {
+        get { return _capienzaMassima; }
+        private set
+        {
+            if (value <= 0)
+            {
+                Console.WriteLine("La capienza massima deve essere un numero positivo.");
+            }
+            _capienzaMassima = value;
+        }
+    }
+
+    public void PrenotaPosti(int prenotazione)
+    {
+        if (Data < DateTime.Now || PostiPrenotati == CapienzaMassima)
+        {
+            Console.WriteLine("Impossibile prenotare i posti richiesti.");
+        }
+        PostiPrenotati += prenotazione;
+    }
+
+    public void DisdiciPosti(int disdicimento)
+    {
+        if (Data < DateTime.Now || PostiPrenotati <= 0)
+        {
+            Console.WriteLine("Impossibile disdire la prenotazione.");
+        }
+        PostiPrenotati -= disdicimento;
+    }
+
+    public override string ToString()
+    {
+        return $"Evento: {Data.ToString("dd/MM/yyyy")} - {Titolo}";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,75 +1,16 @@
-﻿public class Evento
-{
-    public string _titolo;
-    public DateTime _data;
-    public int _capienzaMassima;
-    public int PostiPrenotati { get; private set; }
+﻿Console.WriteLine("Crea un nuovo evento");
 
-    public Evento(string titolo, DateTime data, int capienzaMassima)
-    {
-        Titolo = titolo;
-        Data = data;
-        CapienzaMassima = capienzaMassima;
-        PostiPrenotati = 0;
-    }
+Console.Write("Inserire il nome dell'evento: ");
+string inputTitolo = Console.ReadLine();
 
-    public string Titolo
-    {
-        get { return _titolo; }
-        set
-        {
-            if (string.IsNullOrEmpty(value))
-            {
-                Console.WriteLine("Il titolo non può essere vuoto.");
-            }
-            _titolo = value;
-        }
-    }
-    public DateTime Data
-    {
-        get { return _data; }
-        set
-        {
-            if (value > DateTime.Now)
-            {
-                Console.WriteLine("La data deve essere futura.");
-            }
-            _data = value;
-        }
-    }
-    public int CapienzaMassima
-    {
-        get { return _capienzaMassima; }
-        private set
-        {
-            if (value <= 0)
-            {
-                Console.WriteLine("La capienza massima deve essere un numero positivo.");
-            }
-            _capienzaMassima = value;
-        }
-    }
+Console.Write("Inserire la data in cui si terrà l'evento (dd/MM/yyyy): ");
+DateTime inputData = Convert.ToDateTime(Console.ReadLine());
 
-    public void PrenotaPosti(int prenotazione)
-    {
-        if (Data < DateTime.Now || PostiPrenotati == CapienzaMassima)
-        {
-            Console.WriteLine("Impossibile prenotare i posti richiesti.");
-        }
-        PostiPrenotati += prenotazione;
-    }
+Console.Write("Inserire il numero di posti presenti nel luogo in cui si svolgerà l'evento: ");
+int inputCapacita = Convert.ToInt32(Console.ReadLine());
 
-    public void DisdiciPosti(int disdicimento)
-    {
-        if (Data < DateTime.Now || PostiPrenotati <= 0)
-        {
-            Console.WriteLine("Impossibile disdire i posti richiesti.");
-        }
-        PostiPrenotati -= disdicimento;
-    }
 
-    public override string ToString()
-    {
-        return $"Evento: {Data.ToString("dd/MM/yyyy")} - {Titolo}";
-    }
-}
+Evento mioEvento = new Evento(inputTitolo, inputData, inputCapacita);
+Console.WriteLine(mioEvento);
+
+
