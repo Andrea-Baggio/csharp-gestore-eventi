1546d1ab4aca816740a3959ae5568b5428d43ea4
Andrea-Baggio <andreabaggio800@gmail.com>
2023-03-27 10:16:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿public class Evento
+{
+    public Evento(string titolo, DateTime data, int capienzaMassima, int postiPrenotati)
+    {
+        Titolo = titolo;
+        Data = data;
+        CapienzaMassima = capienzaMassima;
+        PostiPrenotati = postiPrenotati;
+    }
+
+    string Titolo { get; set; }
+    DateTime Data { get; set; }
+    int CapienzaMassima { get;}
+    int PostiPrenotati { get;}
+}
