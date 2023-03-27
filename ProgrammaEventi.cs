19184bb1006d07c39e7f618eb2b4f79994ff158d
Andrea-Baggio <andreabaggio800@gmail.com>
2023-03-27 17:05:22 +0200

    inizio milestone 3

---
+++ ProgrammaEventi.cs
@@ -0,0 +1,15 @@
+﻿public class ProgrammaEventi
+{
+    public string Titolo;
+    public List<Evento> ListaEventi = new();
+
+    public ProgrammaEventi(string titolo, List<Evento> list)
+    {
+        Titolo = titolo;
+        ListaEventi = list;
+    }
+
+    public void AggiungiEvento(Evento evento) => ListaEventi.Add(evento);
+
+    //public Documento? CercaPerCodice(string codice) => ListaDocumenti.FirstOrDefault(documento => documento.Codice == codice);
+}
